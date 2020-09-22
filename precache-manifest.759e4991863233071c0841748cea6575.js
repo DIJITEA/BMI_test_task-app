f998fa8729ec73bb87c71bc82bc021a831024222
DIJITEA <57596699+DIJITEA@users.noreply.github.com>
2020-09-22 18:19:36 +0400

    Updates

---
+++ precache-manifest.759e4991863233071c0841748cea6575.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0405acd15f0faf751f0d81b24dc6d29d",
+    "url": "/DIJITEA/BMI_test_task-app/index.html"
+  },
+  {
+    "revision": "5b3344df5ca86aaa08fa",
+    "url": "/DIJITEA/BMI_test_task-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9f4485b77b92928dbbca",
+    "url": "/DIJITEA/BMI_test_task-app/static/js/2.fa463498.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/DIJITEA/BMI_test_task-app/static/js/2.fa463498.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b3344df5ca86aaa08fa",
+    "url": "/DIJITEA/BMI_test_task-app/static/js/main.4f2c19b3.chunk.js"
+  },
+  {
+    "revision": "e9f995301732c6e22e07",
+    "url": "/DIJITEA/BMI_test_task-app/static/js/runtime-main.947129c6.js"
+  }
+]);
